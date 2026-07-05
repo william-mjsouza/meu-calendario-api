f14e7a43f8754a0d4aa602f14382aaabec577bce
William Souza <william.ws2121@gmail.com>
2026-07-05 14:08:06 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# meu-calendario-api
+API de backend para o aplicativo Meu Calendário, desenvolvida com Spring Boot e PostgreSQL, responsável pelo gerenciamento de eventos e agendamentos recorrentes.
---
+++ src/main/java/com/william/meu_calendario_api/MeuCalendarioApiApplication.java
@@ -0,0 +1,13 @@
+package com.william.meu_calendario_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuCalendarioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuCalendarioApiApplication.class, args);
+	}
+
+}
